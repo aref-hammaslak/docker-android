6c245d786e6e8fdbc962ae3693b792aa8225fe6e
aref-hammaslak <arefhammaslak@gmail.com>
2025-06-05 10:03:13 +0330

    Implement CLI commands for application management and device handling
    - Added CLI commands for starting applications, managing installed versions, and sharing logs.
    - Refactored the device retrieval logic to support multiple device types.
    - Enhanced error handling for unsupported applications and components.

--- cli/src/app.py
+++ cli/src/app.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
+from enum import Enum
 import subprocess
 from typing import Union
-
 import click
 import logging
 import os
-
-from enum import Enum
 
 from src.application import Application
 from src.device import DeviceType
@@ -16,46 +13,71 @@ from src.device.geny_saas import GenySAAS
 from src.helper import convert_str_to_bool, get_env_value_or_raise
 from src.constants import ENV
 from src.logger import log
+from src.config import config
 
 log.init()
 logger = logging.getLogger("App")
 
-
-def get_device(given_input: str) -> Union[Emulator, GenyAWS, GenySAAS, None]:
-    """
-    Get Device object based on given input
-
-    :param given_input: device in string
-    :return: Platform object
-    """
-
-    input_lower = given_input.lower()
-
-    if input_lower == DeviceType.EMULATOR.value.lower():
-        emu_av = get_env_value_or_raise(ENV.EMULATOR_ANDROID_VERSION)
-        emu_img_type = get_env_value_or_raise(ENV.EMULATOR_IMG_TYPE)
-        emu_sys_img = get_env_value_or_raise(ENV.EMULATOR_SYS_IMG)
-
-        emu_device = os.getenv(ENV.EMULATOR_DEVICE, "Nexus 5")
-        emu_data_partition = os.getenv(ENV.EMULATOR_DATA_PARTITION, "550m")
-        emu_additional_args = os.getenv(ENV.EMULATOR_ADDITIONAL_ARGS, "")
-
-        emu_name = os.getenv(ENV.EMULATOR_NAME, "{d}_{v}".format(
-            d=emu_device.replace(" ", "_").lower(), v=emu_av))
-        emu = Emulator(emu_name, emu_device, emu_av, emu_data_partition,
-                       emu_additional_args, emu_img_type, emu_sys_img)
-        return emu
-    elif input_lower == DeviceType.GENY_AWS.value.lower():
-        return GenyAWS()
-    elif input_lower == DeviceType.GENY_SAAS.value.lower():
-        return GenySAAS()
-    else:
-        return None
-
-
+# ------------  cli ------------  
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
 def cli():
     pass
+
+@cli.command()
+@click.argument("app", type=click.Choice([app.value for app in Application.App]))
+def start(app):
+    selected_app = str(app).lower()
+    if selected_app == Application.App.APPIUM.value.lower():
+        start_appium()
+    elif selected_app == Application.App.DEVICE.value.lower():
+        selected_device = start_device()
+        selected_device.keep_alive()
+    elif selected_app == Application.App.DISPLAY_SCREEN.value.lower():
+        start_display_screen()
+    elif selected_app == Application.App.DISPLAY_WM.value.lower():
+        start_display_wm()
+    elif selected_app == Application.App.PORT_FORWARDER.value.lower():
+        start_port_forwarder()
+    elif selected_app == Application.App.VNC_SERVER.value.lower():
+        start_vnc_server()
+    elif selected_app == Application.App.VNC_WEB.value.lower():
+        start_vnc_web()
+    else:
+        logger.error(f"application '{selected_app}' is not supported!")
+
+@cli.command()
+@click.option("--list_installed", is_flag=True)
+@click.option("--install", type=str)
+@click.option("--upgrade", type=str)
+def av(list_installed, install, upgrade):
+    from src.device.version_manager import handle_list_installed, handle_install, handle_upgrade      
+
+    if list_installed:
+        handle_list_installed()
+        return
+    
+    if install:
+        handle_install(install)
+        return
+    
+    if upgrade:
+        handle_upgrade(upgrade)
+        return
+
+class SharedComponent(Enum):
+    LOG = "log"
+@cli.command()
+@click.argument("component", type=click.Choice([component.value for component in SharedComponent]))
+def share(component):
+
+    from src.shared.log_via_web import shared_log
+
+    selected_component = str(component).lower()
+    if selected_component == SharedComponent.LOG.value.lower():
+        shared_log()
+    else:
+        logger.error(f"component '{component}' is not supported!")
+
 
 
 def start_appium() -> None:
@@ -67,7 +89,6 @@ def start_appium() -> None:
     else:
         logger.info("env APPIUM cannot be found, Appium is not started!")
 
-
 def start_device() -> None:
     given_pt = get_env_value_or_raise(ENV.DEVICE_TYPE)
     selected_device = get_device(given_pt)
@@ -77,8 +98,7 @@ def start_device() -> None:
     selected_device.start()
     selected_device.wait_until_ready()
     selected_device.reconfigure()
-    selected_device.keep_alive()
-
+    return selected_device
 
 def start_display_screen() -> None:
     cmd = "/usr/bin/Xvfb"
@@ -90,12 +110,10 @@ def start_display_screen() -> None:
     d_screen = Application("d_screen", cmd, args, False)
     d_screen.start()
 
-
 def start_display_wm() -> None:
     cmd = "/usr/bin/openbox-session"
     d_wm = Application("d_wm", cmd)
     d_wm.start()
-
 
 def start_port_forwarder() -> None:
     import socket
@@ -104,7 +122,6 @@ def start_port_forwarder() -> None:
           f"/usr/bin/socat tcp-listen:5555,bind={local_ip},fork tcp:127.0.0.1:5555"
     pf = Application("port_forwarder", cmd)
     pf.start()
-
 
 def start_vnc_server() -> None:
     cmd = "/usr/bin/x11vnc"
@@ -121,7 +138,6 @@ def start_vnc_server() -> None:
     vnc_server = Application("vnc_web", cmd, args, False)
     vnc_server.start()
 
-
 def start_vnc_web() -> None:
     if convert_str_to_bool(os.getenv(ENV.WEB_VNC)):
         vnc_port = get_env_value_or_raise(ENV.VNC_PORT)
@@ -133,80 +149,33 @@ def start_vnc_web() -> None:
     else:
         logger.info("env WEB_VNC cannot be found, VNC_WEB is not started!")
 
+def get_device(given_input: str) -> Union[Emulator, GenyAWS, GenySAAS, None]:
+    """
+    Get Device object based on given input
 
-@cli.command()
-@click.argument("app", type=click.Choice([app.value for app in Application.App]))
-def start(app):
-    selected_app = str(app).lower()
-    if selected_app == Application.App.APPIUM.value.lower():
-        start_appium()
-    elif selected_app == Application.App.DEVICE.value.lower():
-        start_device()
-    elif selected_app == Application.App.DISPLAY_SCREEN.value.lower():
-        start_display_screen()
-    elif selected_app == Application.App.DISPLAY_WM.value.lower():
-        start_display_wm()
-    elif selected_app == Application.App.PORT_FORWARDER.value.lower():
-        start_port_forwarder()
-    elif selected_app == Application.App.VNC_SERVER.value.lower():
-        start_vnc_server()
-    elif selected_app == Application.App.VNC_WEB.value.lower():
-        start_vnc_web()
+    :param given_input: device in string
+    :return: Platform object
+    """
+    input_lower = given_input.lower()
+    if input_lower == DeviceType.EMULATOR.value.lower():
+        emu_av = config.get("EMULATOR_ANDROID_VERSION")
+        emu_img_type = config.get("EMULATOR_IMG_TYPE")
+        emu_sys_img = config.get("EMULATOR_SYS_IMG")
+
+        emu_device = os.getenv(ENV.EMULATOR_DEVICE, "Nexus 5")
+        emu_data_partition = os.getenv(ENV.EMULATOR_DATA_PARTITION, "550m")
+        emu_additional_args = os.getenv(ENV.EMULATOR_ADDITIONAL_ARGS, "")
+
+        emu_name = f"emulator_{emu_av}"
+        emu = Emulator(emu_name, emu_device, emu_av, emu_data_partition,
+                       emu_additional_args, emu_img_type, emu_sys_img)
+        return emu
+    elif input_lower == DeviceType.GENY_AWS.value.lower():
+        return GenyAWS()
+    elif input_lower == DeviceType.GENY_SAAS.value.lower():
+        return GenySAAS()
     else:
-        logger.error(f"application '{selected_app}' is not supported!")
-
-
-class SharedComponent(Enum):
-    LOG = "log"
-
-
-def shared_log() -> None:
-    if convert_str_to_bool(os.getenv(ENV.WEB_LOG)):
-        from http.server import BaseHTTPRequestHandler, HTTPServer
-
-        log_path = get_env_value_or_raise(ENV.LOG_PATH)
-        log_port = int(get_env_value_or_raise(ENV.WEB_LOG_PORT))
-        logger.info(f"Shared log is enabled! all logs can be found on port '{log_port}'")
-
-        class LogSharedHandler(BaseHTTPRequestHandler):
-            def do_GET(self):
-                # root path
-                if self.path == "/":
-                    html = "<html><body>"
-                    for f in os.listdir(log_path):
-                        html += f"<p><a href=\"{f}\">{f}</a></p>"
-                    html += "</body></html>"
-
-                    self.send_response(200)
-                    self.send_header("Content-type", "text/html")
-                    self.end_headers()
-                    self.wfile.write(html.encode())
-                # open each selected log file
-                else:
-                    p = log_path + self.path
-                    try:
-                        with open(p, "rb") as file:
-                            self.send_response(200)
-                            self.send_header("Content-type", "text/plain")
-                            self.end_headers()
-                            self.wfile.write(file.read())
-                    except FileNotFoundError:
-                        self.send_error(404, "File not found")
-
-        httpd = HTTPServer(('0.0.0.0', log_port), LogSharedHandler)
-        httpd.serve_forever()
-    else:
-        logger.info(f"Shared log is disabled! nothing to do!")
-
-
-@cli.command()
-@click.argument("component", type=click.Choice([component.value for component in SharedComponent]))
-def share(component):
-    selected_component = str(component).lower()
-    if selected_component == SharedComponent.LOG.value.lower():
-        shared_log()
-    else:
-        logger.error(f"component '{component}' is not supported!")
+        return None
 
 
 if __name__ == '__main__':
